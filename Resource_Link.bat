5be25ff86b5c3cd01ca9d80b4a7848cac7d6e4d0
Moazzem Hossain <munnam77@users.noreply.github.com>
2019-11-14 12:40:53 +0900

    Add files via upload

---
+++ Resource_Link.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Here is the GITHUB resource links for making OPENPOSE 3D POSE ESTIMATION.
+echo Openpose 3D baseline
+echo https://github.com/una-dinosauria/3d-pose-baseline
+echo .............................................................................
+echo FCRN Depth Prediction
+echo https://github.com/iro-cp/FCRN-DepthPrediction
+echo .............................................................................
+echo Thank You.
+pause
